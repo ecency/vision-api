39bbaa5e033b6c553c86c457ed198ee795de832d
feruzm <fmiboy@gmail.com>
2024-03-05 09:51:59 +0530

    remove visualblock

--- src/server/handlers/constants.ts
+++ src/server/handlers/constants.ts
@@ -48,7 +48,6 @@ export const bots = [
         "thepimpdistrict",
         "upmewhale",
         "utopian-io",
-        "visualblock",
         "visualbot",
         "warofclans",
         "weed.dispenser",
