e8e30e1b3f9b9af1d1a171a8e5f718376745a1fb
noumantahir <nouman.hawkdev@gmail.com>
2024-07-22 13:39:27 +0500

    fixed Asset split issue

--- src/server/util.ts
+++ src/server/util.ts
@@ -1,55 +1,58 @@
 import { Asset } from "@hiveio/dhive";
-import axios, {AxiosRequestConfig, AxiosResponse, Method} from "axios";
+import axios, { AxiosRequestConfig, AxiosResponse, Method } from "axios";
 
 import express from "express";
 
 export const pipe = (promise: Promise<AxiosResponse>, res: express.Response) => {
-    promise.then(r => {
-        const ddata = typeof r.data === 'number' ? r.data.toString() : r.data;
-        res.status(r.status).send(ddata);
-    }).catch(() => {
-        res.status(500).send("Server Error");
-    });
+  promise.then(r => {
+    const ddata = typeof r.data === 'number' ? r.data.toString() : r.data;
+    res.status(r.status).send(ddata);
+  }).catch(() => {
+    res.status(500).send("Server Error");
+  });
 };
 
 export const baseApiRequest = (url: string, method: Method, headers: any = {}, payload: any = {}, params: any = {}): Promise<AxiosResponse> => {
-    const requestConf: AxiosRequestConfig = {
-        url,
-        method,
-        validateStatus: () => true,
-        responseType: "json",
-        headers: {...headers},
-        data: {...payload},
-        params: {...params}
-    }
+  const requestConf: AxiosRequestConfig = {
+    url,
+    method,
+    validateStatus: () => true,
+    responseType: "json",
+    headers: { ...headers },
+    data: { ...payload },
+    params: { ...params }
+  }
 
-    return axios(requestConf)
+  return axios(requestConf)
 }
 
 
-export const parseToken = (strVal: string | Asset) => {
-    // checks if first part of string is float
-    const regex = /^\-?[0-9]+(e[0-9]+)?(\.[0-9]+)? .*$/;
-    if (!regex.test(strVal)) {
-      return 0;
-    }
-  
-    return Number(parseFloat(strVal.split(' ')[0]));
-  };
+export const parseToken = (val: string) => {
+
+  if (typeof val !== "string") {
+    return 0;
+  }
+  // checks if first part of string is float
+  const regex = /^\-?[0-9]+(e[0-9]+)?(\.[0-9]+)? .*$/;
+  if (!regex.test(val)) {
+    return 0;
+  }
+
+  return Number(parseFloat(val.split(' ')[0]));
+};
 
 
-export const parseAsset = (strVal:string) => {
-    if (typeof strVal !== 'string') {
-      // console.log(strVal);
-      return {
-        amount: 0,
-        symbol: '',
-      };
-    }
-    const sp = strVal.split(' ');
+export const parseAsset = (strVal: string) => {
+  if (typeof strVal !== 'string') {
+    // console.log(strVal);
     return {
-      amount: parseFloat(sp[0]),
-      symbol: Symbol[sp[1]],
+      amount: 0,
+      symbol: '',
     };
+  }
+  const sp = strVal.split(' ');
+  return {
+    amount: parseFloat(sp[0]),
+    symbol: Symbol[sp[1]],
   };
-  
+};
